6e86ca1627948bacf1c0e50e7b20b1fb48a5c798
tristanberris <coding.berris.tristan@gmail.com>
2020-03-23 10:07:01 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,347 @@
+//Animals
+
+class Alligator {
+  constructor(
+      name = 'James',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+}
+
+class Bear {
+  constructor(
+      name = 'Bort',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+}
+
+class Cat {
+  constructor(
+      name = 'Bagels',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+}
+
+class Dog {
+  constructor(
+      name = 'Kirby',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+}
+
+class Elephant {
+  constructor(
+      name = 'Doop',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Frog {
+  constructor(
+      name = 'Froppy',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+
+class Goose {
+  constructor(
+      name = 'Guile',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Hedgehog {
+  constructor(
+      name = 'Sanic',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Iguana {
+  constructor(
+      name = 'Loopy',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Jackal {
+  constructor(
+      name = 'Kevin',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Kiwi {
+  constructor(
+      name = 'Kurt',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Lobster {
+  constructor(
+      name = 'Limmy',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Magpie {
+  constructor(
+      name = 'Devil',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Otter {
+  constructor(
+      name = 'Cooper',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Parrot {
+  constructor(
+      name = 'Salamander',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Rat {
+  constructor(
+      name = 'Rootsy',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Seal {
+  constructor(
+      name = 'Burtstein',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Turkey {
+  constructor(
+      name = 'Terry',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Um {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Vulture {
+  constructor(
+      name = 'Akira',
+      color = '',
+      gender = 'Female',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
+class Walrus {
+  constructor(
+      name = 'Woolsworth',
+      color = '',
+      gender = 'Male',
+      weight = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.teethSize = teethSize;
+  }
+}
+
